079450d4801e956d075adb611c79b86b6c1a8991
Larrygf02 <raulgf_02@hotmail.com>
2020-08-04 19:24:19 -0500

    crear botones flutter

--- lib/src/pages/contador_page.dart
+++ lib/src/pages/contador_page.dart
@@ -29,7 +29,24 @@ class _ContadorPageState extends State<ContadorPage> {
         )
         
       ),
-      floatingActionButton: FloatingActionButton(
+      floatingActionButton: _crearBotones(),
+      //floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+
+  Widget _crearBotones() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        SizedBox(width: 30),
+        FloatingActionButton(child: Icon(Icons.exposure_zero), onPressed: null ),
+        Expanded(child: SizedBox()),
+        FloatingActionButton(child: Icon(Icons.remove), onPressed: null ),
+        SizedBox(width: 5.0),
+        FloatingActionButton(child: Icon(Icons.add), onPressed: null ),
+      ],
+    );
+    /* return FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
           _conteo++;
@@ -38,9 +55,7 @@ class _ContadorPageState extends State<ContadorPage> {
             
           });
         },  
-      ),
-      //floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-    );
-  }
+      ); */
 
+  }
 }
